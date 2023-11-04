e4a3d50b8416c98df27aca32fb8905c70f53e6eb
Leonardo Fragoso <110063706+LeonardoRFragoso@users.noreply.github.com>
2023-11-03 22:53:28 -0300

    Add files via upload

---
+++ Enviar Gmail.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import smtplib
+import email.message
+
+def enviar_email():  
+    corpo_email = """
+    <p>Teste pelo PC</p>
+    <p>Funcionando caralho!</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Teste Note RWE"
+    msg['From'] = 'leonardorfragoso@gmail.com'
+    msg['To'] = 'aesvalentim@gmail.com'
+    password = 'olkdrblvpjbxffqr' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+
+
+# In[ ]:
+
+
+enviar_email()
+
